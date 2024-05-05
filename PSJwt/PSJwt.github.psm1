cfc4812f7b1823e148af327680fc0072abbfaacf
Charlotte.Hong <hunandy14@gmail.com>
2024-05-05 19:54:49 +0900

    新增對 proxy 的支援

--- PSJwt/PSJwt.github.psm1
+++ PSJwt/PSJwt.github.psm1
@@ -1,8 +1,18 @@
 $PSJwtModuleHome = 'https://raw.githubusercontent.com/hunandy14/PsJwt/main/PSJwt'
 
-# private functions
-Invoke-RestMethod "$PSJwtModuleHome/Functions/Convert-ToBase64.ps1" |Invoke-Expression
+$scriptPaths = @(
+    # private functions
+    # public functions
+    'Functions/Convert-ToBase64.ps1',
+    'Functions/Invoke-CommandAndGetBinaryOutput.ps1',
+    'Functions/New-JwtClaimsString.ps1'
+)
 
-# public functions
-Invoke-RestMethod "$PSJwtModuleHome/Functions/Invoke-CommandAndGetBinaryOutput.ps1" |Invoke-Expression
-Invoke-RestMethod "$PSJwtModuleHome/Functions/New-JwtClaimsString.ps1" |Invoke-Expression
+foreach ($scriptPath in $scriptPaths) {
+    $params = @{ Uri = "$PSJwtModuleHome/$scriptPath" }
+    # 檢查 Proxy 環境變數是否設置並且非空
+    if (![string]::IsNullOrWhiteSpace($env:HTTP_PROXY)) {
+        $params['Proxy'] = $env:HTTP_PROXY
+    }
+    Invoke-RestMethod @params | Invoke-Expression
+}
