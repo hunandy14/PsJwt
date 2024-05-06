a964f583b2ac32696e99ebc56698c6e16e8cbeba
Charlotte.Hong <hunandy14@gmail.com>
2024-05-06 17:10:39 +0900

    修正 Utf-8 BOM 的錯誤

--- PSJwt/Functions/New-SecureHexString.ps1
+++ PSJwt/Functions/New-SecureHexString.ps1
@@ -1,9 +1,10 @@
-﻿function New-SecureHexString {
+# 生成特定長度隨機安全字串
+function New-SecureHexString {
     [CmdletBinding(DefaultParameterSetName = "")]
     param(
         [int]$Length = 64
     )
-    $bytes = New-Object byte[] $length
+    $bytes = New-Object byte[] $Length
     $rng = [System.Security.Cryptography.RandomNumberGenerator]::Create()
     $rng.GetBytes($bytes)
     $hex = [BitConverter]::ToString($bytes).Replace('-','').ToLower()
